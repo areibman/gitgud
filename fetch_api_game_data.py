abb33b3d17f3e690f1eee35d02a15d1aba614567
Alex Reibman <areibman@axio.com>
2018-11-07 20:12:01 -0500

    Created player query history function

---
+++ fetch_api_game_data.py
@@ -0,0 +1,91 @@
+import requests
+import time
+from tqdm import tqdm
+import json
+
+API_URL = '.api.riotgames.com/lol/'
+API_KEY = '?api_key=RGAPI-3a4ed2fd-dc8d-4bcc-acd5-360c10f25360'
+MATCH_ID_ENDPOINT = 'match/v3/matchlists/by-account/'
+MATCH_INFORMATION_ENDPOINT = 'match/v3/matches/'
+GAME_TIMELINE_ENDPOINT = 'match/v3/timelines/by-match/'
+
+
+def get_match_history_data(region, champion_id, player_id):
+
+    # ## Step 1
+    # Get the matches
+
+    r = requests.get('https://'+region+API_URL +
+                     MATCH_ID_ENDPOINT+player_id+API_KEY,)
+
+    matches = r.json()
+
+    match_list = []
+
+    for match in matches['matches']:
+        match_list.append(match['gameId'])
+
+    # ## Step 2
+    # Get the game information
+
+    match_info_list = []
+
+    for match in tqdm(match_list):
+        match_info_request =\
+            requests.get('https://'+region+API_URL +
+                         MATCH_INFORMATION_ENDPOINT+str(match)+API_KEY)
+
+        match_info = match_info_request.json()
+
+        particpant_id = ''
+        champion_id = ''
+
+        for participant in match_info['participantIdentities']:
+            if str(participant['player']['accountId']) == player_id:
+                particpant_id = participant['participantId']
+                break
+
+        for participant in match_info['participants']:
+            if participant['participantId'] == particpant_id:
+                champion_id = participant['championId']
+                break
+
+        match_info_list.append({
+            'match_id': match_info['gameId'],
+            'champion_id': champion_id,
+            'participant_id': particpant_id
+        })
+
+        time.sleep(0.2)
+
+    subset_matches = [
+        match for match in match_info_list if match['champion_id'] == champion_id]
+
+    # ## Step 3:
+    # Get participant ID from step 2 and collect data
+
+    player_match_timeline = {}
+
+    for match in tqdm(subset_matches):
+
+        game_timeline_request =\
+            requests.get('https://'+region+API_URL + GAME_TIMELINE_ENDPOINT +
+                         str(match['match_id'])+API_KEY)
+
+        game_timeline = game_timeline_request.json()
+
+        player_frames_list = []
+
+        for frame in game_timeline['frames']:
+            player_frames_list.append(
+                frame['participantFrames'][str(match['participant_id'])])
+
+        player_match_timeline[match['match_id']] = player_frames_list
+
+        time.sleep(0.01)
+
+    return player_match_timeline
+
+
+if __name__ == '__main__':
+    print(get_match_history_data('euw1', '85', '219693852'))
