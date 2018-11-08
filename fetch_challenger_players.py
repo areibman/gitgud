fbaa19b49fec731201b4e5b8815341500a494b38
Alex Reibman <areibman@axio.com>
2018-11-08 00:20:07 -0500

    First run of 5 challengers

---
+++ fetch_challenger_players.py
@@ -0,0 +1,46 @@
+from fetch_api_game_data import get_match_history_data
+import requests
+import json
+from tqdm import tqdm
+
+API_URL = '.api.riotgames.com/lol/'
+API_KEY = '?api_key=RGAPI-3a4ed2fd-dc8d-4bcc-acd5-360c10f25360'
+CHALLENGER_ENDPT = 'league/v3/challengerleagues/by-queue/RANKED_SOLO_5x5'
+ACCOUNT_ID = 'summoner/v3/summoners/'
+
+
+def get_challenger_game_data(region):
+    r = requests.get('https://'+region+API_URL +
+                     CHALLENGER_ENDPT+API_KEY,)
+
+    challengers = r.json()
+
+    challenger_info = []
+
+    for entry in tqdm(challengers['entries'][:5]):
+        r = requests.get('https://' + region+API_URL +
+                         ACCOUNT_ID + entry['playerOrTeamId'] + API_KEY)
+
+        challenger_account_id = r.json()["accountId"]
+
+        challenger_info.append(str(challenger_account_id))
+
+    print('Challenger players being queried:', challenger_info)
+
+    # # Lazy 'memoize'
+    # with open('challengerlist.txt', 'w') as f:
+    #     f.write(challenger_info)
+
+    challenger_data = []
+
+    for player_id in challenger_info:
+        challenger_data.append(get_match_history_data(region, player_id))
+
+    with open("player_history.json", 'w') as f:
+        f.write(json.dumps(challenger_data))
+
+    return challenger_data
+
+
+if __name__ == '__main__':
+    print(get_challenger_game_data('euw1'))
