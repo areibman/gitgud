de46a3160a34b68201dac7b90bfcaa3bcd3e37ad
trebonius <trebonius@worldofwargraphs.com>
2018-11-08 02:26:41 +0100

    Fake data for the graphs, and using a scatter chart

--- GitGudApp/src/components/Hackathon/DataRow.js
+++ GitGudApp/src/components/Hackathon/DataRow.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
 import { Collapse } from "react-collapse";
-import { Line } from "react-chartjs-2";
+import { Line, Scatter } from "react-chartjs-2";
 
 const RowContainer = styled.div`
   display: flex;
@@ -46,12 +46,14 @@ export class DataRow extends React.PureComponent {
 
   render() {
     const { title, data1, data2 } = this.props.row;
+
+    var ajaxResponse = [{ x: 60, y: 0.005 }, { x: 80, y: 0.01 }, { x: 90, y: 0.02 }, { x: 111, y: 0.05 }, { x: 114, y: 0.50 }, { x: 160, y: 0.30 }, { x: 180, y: 0.10 }]
+
     const data = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
       datasets: [
         {
           label: "My First dataset",
-          fill: false,
+          fill: true,
           lineTension: 0.1,
           backgroundColor: "rgba(75,192,192,0.4)",
           borderColor: "rgba(75,192,192,1)",
@@ -68,28 +70,8 @@ export class DataRow extends React.PureComponent {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: "My First dataset",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [12, 70, 10, 91, 15, 35, 90]
+          showLine: true,
+          data: ajaxResponse
         }
       ]
     };
@@ -109,7 +91,7 @@ export class DataRow extends React.PureComponent {
           </InnerRow>
           <InnerRow>
             <Chart>
-              <Line data={data} />
+              <Scatter data={data} />
             </Chart>
           </InnerRow>
           <InnerRow>
